ab06fff582f09f5fdabb2558f673f83308b2966c
Julcsi2 <72194255+Julcsi2@users.noreply.github.com>
2021-08-30 18:17:49 +0200

    Add files via upload

---
+++ WindowsFormsApp13/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormsApp13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Gradovi = new System.Windows.Forms.ListBox();
+            this.Vremenska_prognoza = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ovdje upišite ime grada za koji želite vidjeti prognozu";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Pronađi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nedavno pretraženi gradovi";
+            // 
+            // Gradovi
+            // 
+            this.Gradovi.FormattingEnabled = true;
+            this.Gradovi.ItemHeight = 16;
+            this.Gradovi.Location = new System.Drawing.Point(35, 196);
+            this.Gradovi.Name = "Gradovi";
+            this.Gradovi.Size = new System.Drawing.Size(243, 196);
+            this.Gradovi.TabIndex = 4;
+            this.Gradovi.SelectedIndexChanged += new System.EventHandler(this.Gradovi_SelectedIndexChanged);
+            // 
+            // Vremenska_prognoza
+            // 
+            this.Vremenska_prognoza.FormattingEnabled = true;
+            this.Vremenska_prognoza.ItemHeight = 16;
+            this.Vremenska_prognoza.Location = new System.Drawing.Point(641, 57);
+            this.Vremenska_prognoza.Name = "Vremenska_prognoza";
+            this.Vremenska_prognoza.Size = new System.Drawing.Size(391, 324);
+            this.Vremenska_prognoza.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1121, 450);
+            this.Controls.Add(this.Vremenska_prognoza);
+            this.Controls.Add(this.Gradovi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox Gradovi;
+        private System.Windows.Forms.ListBox Vremenska_prognoza;
+    }
+}
+
---
+++ WindowsFormsApp13/Form1.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string URL = @"https://api.openweathermap.org/data/2.5/weather?q=%name%&units=metric&appid=b0f365a9cfc8b9f80e1e3f9e3b2ff1d9";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Za upisani grad, potraži pripadni json iz kojeg iščitam podatke
+            string html = string.Empty;
+            Gradovi.Items.Add(textBox1.Text);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL.Replace("%name%", textBox1.Text));
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            //Console.WriteLine(html);
+
+            JObject vrijemejson = JObject.Parse(html);
+
+            Vremenska_prognoza.Items.Clear();
+            Vremenska_prognoza.Items.Add(" Grad: "+ textBox1.Text);
+            Vremenska_prognoza.Items.Add(" Temperatura: " + vrijemejson["main"]["temp"].ToString());
+            Vremenska_prognoza.Items.Add(" Osjet: " + vrijemejson["main"]["feels_like"].ToString());
+            Vremenska_prognoza.Items.Add(" Minimalna temperatura: " + vrijemejson["main"]["temp_min"].ToString());
+            Vremenska_prognoza.Items.Add(" Maksimalna temperatura: " + vrijemejson["main"]["temp_max"].ToString());
+            Vremenska_prognoza.Items.Add(" Izgled: " + vrijemejson["weather"][0]["description"].ToString());
+            
+
+            //MessageBox.Show(vrijemejson["weather"][0]["id"].ToString());
+            textBox1.Text = "";
+        }
+
+        private void Gradovi_SelectedIndexChanged(object sender, EventArgs e)   // ako želimo pogledati neki od nedavno pogledanih ponovno
+        {
+            string ime_grada = Gradovi.SelectedItem.ToString();
+
+            string html = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL.Replace("%name%", ime_grada));
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            JObject vrijemejson = JObject.Parse(html);
+
+            Vremenska_prognoza.Items.Clear();
+            Vremenska_prognoza.Items.Add(" Grad: " + ime_grada);
+            Vremenska_prognoza.Items.Add(" Temperatura: " + vrijemejson["main"]["temp"].ToString());
+            Vremenska_prognoza.Items.Add(" Osjet: " + vrijemejson["main"]["feels_like"].ToString());
+            Vremenska_prognoza.Items.Add(" Minimalna temperatura: " + vrijemejson["main"]["temp_min"].ToString());
+            Vremenska_prognoza.Items.Add(" Maksimalna temperatura: " + vrijemejson["main"]["temp_max"].ToString());
+            Vremenska_prognoza.Items.Add(" Izgled: " + vrijemejson["weather"][0]["description"].ToString());
+
+        }
+    }
+}
